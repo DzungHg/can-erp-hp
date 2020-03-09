ef7c13504756eed2db497fc282c5636124d46247
DzungHg <hg.dzung@gmail.com>
2020-03-09 17:17:09 +0700

    Customer-Tiếp

--- oe-op/server/Pages/AddTblSoCustomer.razor.cs
+++ oe-op/server/Pages/AddTblSoCustomer.razor.cs
@@ -11,23 +11,27 @@ namespace CanErp2.Pages
     {
         public void FillSomeDatas(int? contactID)
         {
-            const int totalLengthOfCustomerID = 9; //gồm cả ký tự C
+            //Tổng char của customerID là 10
+            const int totalLengthOfCustomerID = 10; //gồm cả ký tự C
             int total0CharAdded;
-            string str0;
+            string str0 = "C"; //Thêm C trước
 
-            total0CharAdded = totalLengthOfCustomerID - contactID.ToString().Length - 1; //Trừ 1 là bỏ C để thêm tổng số 0
+            total0CharAdded = totalLengthOfCustomerID - contactID.ToString().Length - 1; //Trừ đi C đã add vô rồi
 
             for (int i = 0; i < total0CharAdded; i++)
             {
-                str0 = string.Create()
+                str0 += "0";
             }
 
-            this.InitCustomerID = string.Format("C000{0}",  contactID);
-            var addressContact = getTblGnAddressBooksResult.Where(x => x.AddressBook_SEQ == contactID).FirstOrDefault();
+            if (contactID > 0)
+            {
+                this.InitCustomerID = string.Format("{0}{1}", str0, contactID);
+                var addressContact = getTblGnAddressBooksResult.Where(x => x.AddressBook_SEQ == contactID).FirstOrDefault();
 
-            //điền dữ liệu
-            this.InitCustFirstName = addressContact.FirstName;
-            this.InitCustLastName = addressContact.LastName;
+                //điền dữ liệu
+                this.InitCustFirstName = addressContact.FirstName;
+                this.InitCustLastName = addressContact.LastName;
+            }
            
         }
         public string InitCustomerID { get; set; }
